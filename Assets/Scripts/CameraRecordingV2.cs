29e6dfe82957f6ac04145f8f11d4ab27528edacd
Neil Del Gallego <neil.delgallego@dlsu.edu.ph>
2025-03-06 17:01:02 +0800

    Improvements in dataset gathering in FCG.

--- Assets/Scripts/CameraRecordingV2.cs
+++ Assets/Scripts/CameraRecordingV2.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using UnityEngine;
+using UnityEngine.Perception.GroundTruth;
 using UnityEngine.SceneManagement;
 using Debug = UnityEngine.Debug;
 
@@ -15,6 +16,8 @@ public class CameraRecordingV2 : MonoBehaviour
     private int frameCount = DomainParameters.OFFSET;
 
     private string basePath = "X:/GithubProjects/NeuralNets-SynthWorkplace_V3/Recordings/";
+    //NOTE: When running on EXE, images are saved in "C:/Users/delgallegon/AppData/LocalLow/DefaultCompany/NeuralNets-SynthWorkplace_V3/";
+
     private static CameraRecordingV2 sharedInstance;
 
     public static CameraRecordingV2 Instance
@@ -24,6 +27,8 @@ public class CameraRecordingV2 : MonoBehaviour
             return sharedInstance;
         }
     }
+
+    [SerializeField] private PerceptionCamera perceptionCamera;
 
     void Awake()
     {
@@ -43,13 +48,21 @@ public class CameraRecordingV2 : MonoBehaviour
     void Update()
     {
         frameCount++;
-        //this.imageSynthesisInstance.Save("frame_"+this.frameCount+".png", 256, 256, this.basePath);
 
-        // if (frameCount > DomainParameters.MAX_IMAGES_TO_SAVE)
-        // {
-        //     Debug.Log("Captured enough frames. Quitting application.");
-        //     Application.Quit();
-        // }
+        //allow render to finalize lighting and post-processing first.
+        const int offsetFrames = 20;
+        if (frameCount < offsetFrames)
+        {
+            return;
+        }
+
+        this.perceptionCamera.RequestCapture();
+
+        if (frameCount - offsetFrames > DomainParameters.MAX_IMAGES_TO_SAVE)
+        {
+            Debug.Log("Captured enough frames. Quitting application.");
+            Application.Quit();
+        }
     }
 
     public int GetCurrentFrameCount()
