04619e56d108399754b26368e45d546eb0ae7793
Neil Del Gallego <neil.delgallego@dlsu.edu.ph>
2025-03-06 17:03:01 +0800

    Update DomainParameters.cs

--- Assets/Scripts/DomainParameters.cs
+++ Assets/Scripts/DomainParameters.cs
@@ -6,7 +6,7 @@ public class DomainParameters
 {
     public const int OFFSET = 0;
     public const int REFRESH_SCENE_PER_FRAME = 100;
-    public const int MAX_IMAGES_TO_SAVE = 1000;
+    public const int MAX_IMAGES_TO_SAVE = 15000;
 
     // public static readonly Vector3 MIN_POS_CONFIG = new Vector3(-340.0f, 40.0f, -265.0f);
     // public static readonly Vector3 MAX_POS_CONFIG = new Vector3(350.0f, 80.0f, 105.0f);
